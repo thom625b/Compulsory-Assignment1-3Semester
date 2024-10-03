aa0286cd0e7ef0de23ec5fa175167f2ce5f8e9eb
Thomas Olesen <thomasolesen0505@hotmail.com>
2024-10-03 12:46:02 +0200

    enum

--- server/DataAccess/Models/Order.cs
+++ server/DataAccess/Models/Order.cs
@@ -4,36 +4,36 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 
-namespace DataAccess.Models;
-
-[Table("orders")]
-[Index("CustomerId", Name = "IX_orders_customer_id")]
-public partial class Order
+namespace DataAccess.Models
 {
-    [Key]
-    [Column("id")]
-    public int Id { get; set; }
+    [Table("orders")]
+    [Index("CustomerId", Name = "IX_orders_customer_id")]
+    public partial class Order
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
 
-    [Column("order_date")]
-    public DateTime OrderDate { get; set; }
+        [Column("order_date")]
+        public DateTime OrderDate { get; set; }
 
-    [Column("delivery_date")]
-    public DateOnly? DeliveryDate { get; set; }
+        [Column("delivery_date")]
+        public DateOnly? DeliveryDate { get; set; }
 
-    [Column("status")]
-    [StringLength(50)]
-    public string Status { get; set; } = null!;
+        [Column("status")]
+        public OrderStatus Status { get; set; }  // Use the enum here
 
-    [Column("total_amount")]
-    public double TotalAmount { get; set; }
+        [Column("total_amount")]
+        public double TotalAmount { get; set; }
 
-    [Column("customer_id")]
-    public int? CustomerId { get; set; }
+        [Column("customer_id")]
+        public int? CustomerId { get; set; }
 
-    [ForeignKey("CustomerId")]
-    [InverseProperty("Orders")]
-    public virtual Customer? Customer { get; set; }
+        [ForeignKey("CustomerId")]
+        [InverseProperty("Orders")]
+        public virtual Customer? Customer { get; set; }
 
-    [InverseProperty("Order")]
-    public virtual ICollection<OrderEntry> OrderEntries { get; set; } = new List<OrderEntry>();
+        [InverseProperty("Order")]
+        public virtual ICollection<OrderEntry> OrderEntries { get; set; } = new List<OrderEntry>();
+    }
 }
