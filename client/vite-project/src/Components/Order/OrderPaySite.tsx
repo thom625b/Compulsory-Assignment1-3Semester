738895ac402f81a4149fdc32afc6b3a18f0165e4
Thomas Olesen <thomasolesen0505@hotmail.com>
2024-10-05 13:02:12 +0200

    Creating customer function working

--- client/vite-project/src/Components/Order/OrderPaySite.tsx
+++ client/vite-project/src/Components/Order/OrderPaySite.tsx
@@ -1,95 +1,91 @@
-const OrderPaySite = () =>
-{
-    return(
+import React, {useState} from "react";
+import {Api} from "../../Api.ts";
+
+
+const OrderPaySite = () => {
+    const api = new Api();
+    const [name, setName] = useState('');
+    const [address, setAddress] = useState('');
+    const [phone, setPhone] = useState('');
+    const [email, setEmail] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [successMessage, setSuccessMessage] = useState('');
+
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        if (!name || !address || !phone || !email) {
+            setErrorMessage("All fields are required.");
+            setSuccessMessage('');
+            return;
+        }
+
+        const newCustomer = {
+            name,
+            address,
+            phone,
+            email,
+        };
+
+        try {
+            await api.api.customerCreateCustomer(newCustomer);
+            setSuccessMessage("Customer created successfully");
+            setErrorMessage('');
+            setName('');
+            setAddress('');
+            setPhone('');
+            setEmail('');
+        } catch (error) {
+            setErrorMessage("Failed to create customer")
+            setSuccessMessage('');
+        }
+    }
+
+    return (
         <>
-            <div className="BillingForm__InputLine-gc3cew-5 kkvEMA"><label
-                className="Text__Label-nmegcg-2 hNgmsK BillingForm__InputField-gc3cew-2 lckdxV">
-                <div className="Text__LabelText-nmegcg-3 woWuc">E-mail adresse</div>
-                <div className="Text__InputWrapper-nmegcg-0 imrzAW"><input name="email" type="text"
-                                                                           aria-label="E-mail adresse"
-                                                                           className="Text__Input-nmegcg-1 iKlnta BillingForm__InputField-gc3cew-2 lckdxV"
-                                                                           value=""/>
-                    <div className="Text__ValidationGraphic-nmegcg-5 hiGhoP"></div>
-                </div>
-            </label>
-                <div className="BillingForm__Note-gc3cew-6 eUIjqi">Vi sender ordreinformation og ordrebekræftelse til
-                    din email
-                </div>
-            </div>
-            <div className="grid__Row-sc-18ibg2x-2 iNUwCA">
-                <div className="grid__Column-sc-18ibg2x-3 hPaRCT">
-                    <div className="BillingForm__InputLine-gc3cew-5 kkvEMA"><label
-                        className="Text__Label-nmegcg-2 hNgmsK BillingForm__InputField-gc3cew-2 lckdxV">
-                        <div className="Text__LabelText-nmegcg-3 woWuc">Fornavn</div>
-                        <div className="Text__InputWrapper-nmegcg-0 imrzAW"><input aria-label="Fornavn"
-                                                                                   name="billingAddress.firstName"
-                                                                                   type="text" maxLength={30}
-                                                                                   className="Text__Input-nmegcg-1 iKlnta BillingForm__InputField-gc3cew-2 lckdxV"
-                                                                                   value=""/>
-                            <div className="Text__ValidationGraphic-nmegcg-5 hiGhoP"></div>
-                        </div>
-                    </label></div>
-                </div>
-                <div className="grid__Column-sc-18ibg2x-3 hPaRCT">
-                    <div className="BillingForm__InputLine-gc3cew-5 kkvEMA"><label
-                        className="Text__Label-nmegcg-2 hNgmsK BillingForm__InputField-gc3cew-2 lckdxV">
-                        <div className="Text__LabelText-nmegcg-3 woWuc">Efternavn</div>
-                        <div className="Text__InputWrapper-nmegcg-0 imrzAW"><input aria-label="Efternavn"
-                                                                                   name="billingAddress.lastName"
-                                                                                   type="text" maxLength={30}
-                                                                                   className="Text__Input-nmegcg-1 iKlnta BillingForm__InputField-gc3cew-2 lckdxV"
-                                                                                   value=""/>
-                            <div className="Text__ValidationGraphic-nmegcg-5 hiGhoP"></div>
-                        </div>
-                    </label></div>
-                </div>
-            </div>
+            <h1 className="text-lg font-bold">Create Customer</h1>
+            <form onSubmit={handleSubmit} className="space-y-4">
+                {errorMessage && <div className="text-red-500">{errorMessage}</div>}
+                {successMessage && <div className="text-green-500">{successMessage}</div>}
 
-            <div className="grid__Row-sc-18ibg2x-2 iNUwCA">
-                <div className="grid__Column-sc-18ibg2x-3 jcJEZr">
-                    <div className="BillingForm__InputLine-gc3cew-5 kkvEMA"><label
-                        className="Text__Label-nmegcg-2 hNgmsK BillingForm__InputField-gc3cew-2 lckdxV">
-                        <div className="Text__LabelText-nmegcg-3 woWuc">Adresse</div>
-                        <div className="Text__InputWrapper-nmegcg-0 imrzAW"><input aria-label="Adresse"
-                                                                                   name="billingAddress.addressLine"
-                                                                                   type="text" maxLength={30}
-                                                                                   className="Text__Input-nmegcg-1 iKlnta BillingForm__InputField-gc3cew-2 lckdxV"
-                                                                                   value=""/>
-                            <div className="Text__ValidationGraphic-nmegcg-5 hiGhoP"></div>
-                        </div>
-                    </label></div>
-                </div>
-                <div className="grid__Column-sc-18ibg2x-3 ejwWyT">
-                    <div className="BillingForm__InputLine-gc3cew-5 kkvEMA"><label
-                        className="Text__Label-nmegcg-2 hNgmsK BillingForm__InputField-gc3cew-2 lckdxV">
-                        <div className="Text__LabelText-nmegcg-3 woWuc">Hus nr.</div>
-                        <div className="Text__InputWrapper-nmegcg-0 imrzAW"><input aria-label="Hus nr."
-                                                                                   name="billingAddress.addressNumber"
-                                                                                   type="text" maxLength={30}
-                                                                                   className="Text__Input-nmegcg-1 iKlnta BillingForm__InputField-gc3cew-2 lckdxV"
-                                                                                   value=""/>
-                            <div className="Text__ValidationGraphic-nmegcg-5 hiGhoP"></div>
-                        </div>
-                    </label></div>
-                </div>
-            </div>
-            <div className="BillingForm__InputLine-gc3cew-5 kkvEMA"><label
-                className="Text__Label-nmegcg-2 hNgmsK BillingForm__InputField-gc3cew-2 cIsZuK">
-                <div className="Text__LabelText-nmegcg-3 woWuc">Mobilnummer</div>
-                <div className="Text__InputWrapper-nmegcg-0 imrzAW"><input aria-label="Mobilnummer" name="phoneNumber"
-                                                                           type="tel" maxLength={8}
-                                                                           className="Text__Input-nmegcg-1 iKlnta BillingForm__InputField-gc3cew-2 cIsZuK"
-                                                                           value=""/>
-                    <div className="Text__ValidationGraphic-nmegcg-5 hiGhoP"></div>
-                </div>
-            </label>
-                <div className="BillingForm__Note-gc3cew-6 eUIjqi"> Vi vil kun bruge dit mobil nr. hvis der opstår
-                    problemer med din ordre.
-                </div>
-            </div>
+                <label className="input input-bordered flex items-center gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="h-4 w-4 opacity-70">
+                        <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z"/>
+                        <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z"/>
+                    </svg>
+                    <input type="text" className="grow" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
+                </label>
 
+                <label className="input input-bordered flex items-center gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="h-4 w-4 opacity-70">
+                        <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z"/>
+                        <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z"/>
+                    </svg>
+                    <input type="text" className="grow" placeholder="Address" value={address} onChange={(e) => setAddress(e.target.value)} />
+                </label>
+
+                <label className="input input-bordered flex items-center gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="h-4 w-4 opacity-70">
+                        <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z"/>
+                        <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z"/>
+                    </svg>
+                    <input type="text" className="grow" placeholder="Phone" value={phone} onChange={(e) => setPhone(e.target.value)} />
+                </label>
+
+                <label className="input input-bordered flex items-center gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="h-4 w-4 opacity-70">
+                        <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z"/>
+                        <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z"/>
+                    </svg>
+                    <input type="text" className="grow" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                </label>
+                <button className="w-full bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600 transition duration-200">
+                    Create Customer
+                </button>
+            </form>
         </>
-    )
+    );
 };
 
 export default OrderPaySite;
